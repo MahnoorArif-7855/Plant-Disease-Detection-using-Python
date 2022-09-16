4272f31d3778a782cdf5c715a04a4b818ce5a0ba
MahnoorArif-7855 <mahnoorarif5587@gmail.com>
2022-09-16 13:02:02 -0700

    Plant dieasse detection

---
+++ app/android/app/src/main/kotlin/com/example/susya/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.FYP.SUSya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/lib/generated_plugin_registrant.dart
@@ -0,0 +1,32 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:camera_web/camera_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:share_plus_web/share_plus_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  CameraPlugin.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  SharePlusPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
